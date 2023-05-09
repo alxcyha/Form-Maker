d973f14647c8fdec07b3923b7888ee14f6753554
alxcyha <villaflor.aliciamargaret@gmail.com>
2023-05-09 21:53:06 +0800

    updated forms

--- script.js
+++ script.js
@@ -23,6 +23,10 @@ function createBox() {
 
     switch(selectedValue) {
         
+        case 1:
+            appendText();
+            break;
+
         case 2:
             appendRadio();
             break;
@@ -33,39 +37,21 @@ function createBox() {
     }
 
 }
+/**
+ * A function will be invoked if the selected value(option) is for text
+ */
 
-
-function appendCheckBox() {
-
+function appendText(){
     // Create a new container div
     const container = document.createElement('div');
     container.className = 'd-flex flex-column container px-4 py-3 boxes bg-light';
 
-    // Create a new container div
+    // Create a new input element for text
     const input = document.createElement('input');
     input.type = 'text';
-    input.placeholder = 'Question';
+    input.placeholder = 'input text here';
     input.className = 'p-2';
     input.style.width = '100%';
-
-    // Add the input to the container
-    container.append(input);
-
-    // Create a checkbox container
-    const checkContainer = document.createElement('div');
-    checkContainer.className = 'd-flex gap-1 flex-column mt-3'
-
-    // Create checkbox
-    const checkbox1 = createCheckBox('1', 'test');
-    checkContainer.appendChild(checkbox1);
-
-    const checkbox2 = createCheckBox('2', 'test');
-    checkContainer.appendChild(checkbox2);
-
-    const checkbox3 = createCheckBox('3', 'test');
-    checkContainer.appendChild(checkbox3);
-
-    container.appendChild(checkContainer);
 
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('btn', 'btn-danger', 'delete-button');
@@ -84,40 +70,17 @@ function appendCheckBox() {
     deleteButton.style.borderColor = '#9932CC';
     });
 
-    /**
-     * Add an event listener in this button
-     */
     deleteButton.addEventListener('click', () => {
         deleteContainer(container);
     });
-
+    container.appendChild(input);
     container.appendChild(deleteButton);
 
-    // Append the new container div to the #boxes-uwu element
+    // Append the new container div 
     const box = document.getElementById('display');
-    box.appendChild(container);
+    box.append(container);
 
-}
-
-/**
- * A function that creates the checkbutton
- * @param {string} value string representing the value for the checkbox
- * @param {string} label string representing the label for the checkbox
- */
-function createCheckBox(value, label) {
-
-    console.log(label);
-    const checkboxLabel = document.createElement('label');
-    const checkBox = document.createElement('input');
-
-    checkBox.type = 'checkbox';
-    checkBox.value = value;
-    
-    // Adding
-    checkboxLabel.append(checkBox);
-    checkboxLabel.append(label);
-
-    return checkBox;
+    console.log(document.getElementById('display'));
 
 }
 
@@ -158,9 +121,6 @@ function appendRadio() {
     deleteButton.style.borderColor = '#9932CC';
     });
 
-    /**
-     * Add an event listener in this button
-     */
     deleteButton.addEventListener('click', () => {
         deleteContainer(container);
     });
@@ -174,7 +134,7 @@ function appendRadio() {
     container.appendChild(document.createElement('br'));
     container.appendChild(deleteButton);
 
-    // Append the new container div to the #boxes-uwu element
+    // Append the new container div 
     const box = document.getElementById('display');
     box.append(container);
 
@@ -182,12 +142,7 @@ function appendRadio() {
 
 }
 
-/**
- * 
- * @param {string} id 
- * @param {string} label 
- * @returns 
- */
+
 function createRadio(id, label) {
   const radio = document.createElement('input');
   radio.type = 'radio';
@@ -206,6 +161,92 @@ function createRadio(id, label) {
   radioDiv.appendChild(radioLabel);
 
   return radioDiv;
+}
+function appendCheckBox() {
+
+    // Create a new container div
+    const container = document.createElement('div');
+    container.className = 'd-flex flex-column container px-4 py-3 boxes bg-light';
+
+    // Create a new container div
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Question';
+    input.className = 'p-2';
+    input.style.width = '100%';
+
+    // Add the input to the container
+    container.appendChild(input);
+
+    // Create a checkbox container
+    const checkContainer = document.createElement('div');
+    checkContainer.className = 'd-flex gap-1 flex-column mt-3'
+
+    // Create checkbox
+    const check1 = createCheckBox('1', 'test');
+    const check2 = createCheckBox('2', 'test');
+    const check3 = createCheckBox('3', 'test');
+
+    //append
+    checkContainer.appendChild(check1);
+    checkContainer.appendChild(check2);
+    checkContainer.appendChild(check3);
+
+    container.appendChild(checkContainer);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn', 'btn-danger', 'delete-button');
+    deleteButton.style.backgroundColor = '#9932CC';
+    deleteButton.style.borderColor = '#9932CC';
+    deleteButton.style.float = 'right';
+    deleteButton.innerText = 'Delete';
+
+    deleteButton.addEventListener('mouseover', () => {
+        deleteButton.style.backgroundColor = '#8B008B';
+        deleteButton.style.borderColor = '#8B008B';
+    });
+
+    deleteButton.addEventListener('mouseout', () => {
+    deleteButton.style.backgroundColor = '#9932CC';
+    deleteButton.style.borderColor = '#9932CC';
+    });
+
+   
+    deleteButton.addEventListener('click', () => {
+        deleteContainer(container);
+    });
+
+    container.appendChild(deleteButton);
+
+  
+    const box = document.getElementById('display');
+    box.appendChild(container);
+
+}
+
+/**
+ * A function that creates the checkbutton*/
+
+function createCheckBox(value, label) {
+
+    
+    const checkboxLabel = document.createElement('label');
+    const checkBox = document.createElement('input');
+
+    checkBox.type = 'checkbox';
+    checkBox.value = value;
+    checkBox.className = 'me-2';
+    checkboxLabel.innerText = label;
+    
+    // Adding
+    const checkForm = document.createElement('div');
+    checkForm.className = "form-check p-0";
+
+    checkForm.appendChild(checkBox)
+    checkForm.appendChild(checkboxLabel);
+
+    return checkForm;
+
 }
 
 /**
